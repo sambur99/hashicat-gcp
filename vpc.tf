62f5b4ec1e23c63cebc1ea4b51a9756f5d588b32
Sam Burgess <samburgess@hotmail.com>
2021-05-19 16:55:36 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,13 @@
-network_name = "sam-network"
-project_id = var.project
-subnets = [
-  {
-    subnet_name   = "sam-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+module "network" {
+  source       = "app.terraform.io/samburgess-traing/network/google"
+  version      = "2.5.0"
+  network_name = "sam-network"
+  project_id   = var.project
+  subnets = [
+    {
+      subnet_name   = "sam-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
+}
